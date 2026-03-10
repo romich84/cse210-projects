60f7452978a01484d7f25328f08882f536fb9d34
Romina Chazarreta <rchazarreta@byupathway.edu>
2026-03-10 01:16:06 -0300

    I add Learning Activity: Abstraction - week 2

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,19 @@
+﻿namespace Resumes;
+
+public class Job
+{
+   public string _company;
+   public string _jobTitle;
+   public int startYear;
+   public int endYear;
+
+   public void Display()
+   {
+      Console.WriteLine($"{_company}");
+   }
+
+   public void DisplayJobDetails()
+   {
+      Console.WriteLine($"{_jobTitle} ({_company}) {startYear}-{endYear}");
+   }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2.startYear = 2022;
+        job2.endYear = 2023;
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
