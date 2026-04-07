e836ed3637100a84a166cf757b7fb3000cca23b8
Romina Chazarreta <rchazarreta@byupathway.edu>
2026-04-06 22:46:19 -0300

    I add the learning activity: Polymorphism

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,26 @@
+﻿namespace Shapes;
+
+public class Circle : Shape
+{
+    private double _radius;
+    
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        //Square square = new Square("blue", 10);
+        //Console.WriteLine(square.GetColor());
+        //Console.WriteLine(square.GetArea());
+        //Rectangle rectangle = new Rectangle("red", 10, 5);
+        //Console.WriteLine(rectangle.GetColor());
+        //Console.WriteLine(rectangle.GetArea());
+        //Circle circle = new Circle("yellow", 5);
+        //Console.WriteLine(circle.GetColor());
+        //Console.WriteLine(circle.GetArea());
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 10));
+        shapes.Add(new Rectangle("red",10, 5));
+        shapes.Add(new Circle("yellow", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            Console.WriteLine(color);
+            double area = shape.GetArea();
+            Console.WriteLine(area);
+        }
+        
     }
 }
